f50e639fd3c4fef03945ecae4973bfca3d75d993
abed7helmi <abed.helmi@yahoo.fr>
2023-11-16 15:04:24 +0100

    first contact with chatgpt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { GptComponent } from './gpt/gpt.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {ReactiveFormsModule} from "@angular/forms";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gpt/gpt.component.ts
+++ src/app/gpt/gpt.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-gpt',
@@ -7,10 +8,13 @@ import {FormBuilder, FormGroup} from "@angular/forms";
   styleUrls: ['./gpt.component.css']
 })
 export class GptComponent implements OnInit {
-
+  result : any ;
   queryFormGroup !: FormGroup
+  messages = [
+    {role : "system" , content:"You are a helpful assistant."}
+  ];
 
-  constructor(private fb : FormBuilder) { }
+  constructor(private fb : FormBuilder , private httpClient : HttpClient) { }
 
   ngOnInit(): void {
 
@@ -21,6 +25,26 @@ export class GptComponent implements OnInit {
   }
 
   handleAskGPT() {
+    let url = "https://api.openai.com/v1/chat/completions " ;
+    let httpHeaders = new HttpHeaders().set("Authorization","Bearer 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")
+
+    this.messages.push({
+      role: "user" , content: this.queryFormGroup.value.query
+    })
+
+    let payload = {
+      model : "gpt-3.5-turbo" ,
+      messages : this.messages
+    }
+    this.httpClient.post(url,payload,{headers:httpHeaders}).subscribe({
+      next : (resp) =>{
+        this.result = resp
+      },
+      error : (err)=>{
+        this.result = err
+
+      }
+    })
 
   }
 }
